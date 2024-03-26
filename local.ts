0268600a21110cb85c8bfa0dfd675c5dd49612cf
wbh91128 <wbh91128_win@outlook.com>
2024-03-26 11:25:02 +0800

    Create local.ts

---
+++ local.ts
@@ -0,0 +1,5 @@
+async hanleProjectTypeChange(projectType: ProjectTypeEnum) {
+    localStorage.setItem(CREATE_PROJECT_TYPE, projectType);
+    this.updateCustomForm();
+    this.initDefaultNamespaceId();
+}
