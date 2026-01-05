bcb3ce98b5906f80ce7ced9c8553ab81d925c3f2
wbh91128 <wbh91128_dev@outlook.com>
2026-01-05 16:48:29 +0800

    add log function

--- local.ts
+++ local.ts
@@ -1,5 +1,14 @@
 function getSql(tableName:string) {
-    return "select * from " + tableName + " where username = " + tableName";
+    console.debug(tableName);
+    return "select * from " + tableName + " where username = " + tableName
+}
+
+export function getUserTheme(themeId: string) {
+    return "Default Theme" + themeId;
+}
+
+export function log(message:string) {
+    console.log(message)
 }
 
 export default getSql
